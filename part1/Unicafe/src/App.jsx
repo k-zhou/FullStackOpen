0f09b094b3bcaf66649cceb6b6bf312aa16e7fb0
k-zhou <kejin.zhou@aalto.fi>
2024-11-20 18:21:50 +0200

    finishes ex. 1.9.

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -9,39 +9,47 @@ const Statistics = ({statisticsObj}) => {
   const average  = (good - bad) / denom
   const positive = good / denom
 
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td>Good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>Neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>Bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>All</td>
-            <td>{all}</td>
-          </tr>
-          <tr>
-            <td>Average</td>
-            <td>{average}</td>
-          </tr>
-          <tr>
-            <td>Positive</td>
-            <td>{positive}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  if (all == 0)
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
   )
+  else
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>Good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>Neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>Bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>All</td>
+              <td>{all}</td>
+            </tr>
+            <tr>
+              <td>Average</td>
+              <td>{average}</td>
+            </tr>
+            <tr>
+              <td>Positive</td>
+              <td>{positive}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
 }
 // const Button = ({eventHandler}) => {
   // return (
