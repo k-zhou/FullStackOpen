71dd02da69ca4f54e3e6823c09617ed0e9127ea9
k-zhou <kejin.zhou@aalto.fi>
2024-11-20 17:26:54 +0200

    finishes ex. 1.6.

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,5 +1,34 @@
 import { useState } from 'react'
 
+const Statistics = ({statisticsObj}) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Good</td>
+            <td>{statisticsObj.good}</td>
+          </tr>
+          <tr>
+            <td>Neutral</td>
+            <td>{statisticsObj.neutral}</td>
+          </tr>
+          <tr>
+            <td>Bad</td>
+            <td>{statisticsObj.bad}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+// const Button = ({eventHandler}) => {
+  // return (
+  //   <p>{text}</p>
+  // )
+// }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +37,11 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}      >Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}        >Bad</button>
+      <Statistics statisticsObj={{good, neutral, bad}} />
     </div>
   )
 }
