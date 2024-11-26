09bab30e6e8853ed329c523958920b5cb9833258
k-zhou <kejin.zhou@aalto.fi>
2024-11-26 14:16:07 +0200

    adds error prints to personsService

--- part2/Phonebook/src/personsService.js
+++ part2/Phonebook/src/personsService.js
@@ -3,19 +3,27 @@ import axios from 'axios'
 const SERVER_ADDRESS = 'http://localhost:3001/persons'
 
 const getAll = () => {
-    return axios.get(SERVER_ADDRESS)
+    return axios
+            .get(SERVER_ADDRESS)
+            .catch(response => console.log(`GET Error: ${response.data}`))
 }
 
 const create = (newObj) => {
-    return axios.post(SERVER_ADDRESS, newObj)
+    return axios
+            .post(SERVER_ADDRESS, newObj)
+            .catch(response => console.log(`POST Error: ${response.data}`))
 }
 
 const update = (newObj, id) => {
-    return axios.put(`${SERVER_ADDRESS}/${id}`, newObj).catch(response => `$Error: ${response.data}`)
+    return axios
+            .put(`${SERVER_ADDRESS}/${id}`, newObj)
+            .catch(response => console.log(`PUT Error: ${response.data}`))
 }
 
 const remove = (id) => {
-    return axios.delete(`${SERVER_ADDRESS}/${id}`).catch(response => `$Error: ${response.data}`)
+    return axios
+            .delete(`${SERVER_ADDRESS}/${id}`)
+            .catch(response => console.log(`DELETE Error: ${response.data}`))
 }
 
 export default {
