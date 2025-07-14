d075b1176a75483eb9310ce5ad35c6540bfc5aed
Kejin Zhou <kejin.zhou@aalto.fi>
2025-07-14 15:26:42 +0300

    finishes 3.8*. changes morgan to use a custom format; GDPR warning

--- server/app-server.ts
+++ server/app-server.ts
@@ -10,7 +10,12 @@ const PORT = 3001;
 
 const app = express();
 
-app.use(morgan("tiny"));
+// Sets up Morgan
+// Defines a new text token for showing the payload
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+// app.use(morgan("tiny"));
+// The preset "tiny" corresponds to ":method :url :status :res[content-length] - :response-time ms"
+app.use(morgan(`:method ":url" [Status :status] [Content-length :res[content-length]] [:response-time ms] Payload :body`));
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!<h1/>");
