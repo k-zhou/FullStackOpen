1bac54959cd45febf1511e8aeeb18869dae5eda9
Kejin Zhou <kejin.zhou@aalto.fi>
2025-07-16 12:56:19 +0300

    fix frontend main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import './index.css'
 import App from './App.tsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 const renderApp = () => {
   root.render(
