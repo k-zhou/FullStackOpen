a3d5a12574c8842f285973e0de94a6617b650188
Kejin Zhou <kejin.zhou@aalto.fi>
2025-07-16 10:50:30 +0300

    updates compose and vite config: client to use env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( ({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  // Something about not recommended exposing all env variables
+  // "process.env": env
+  return {
+    define: {
+      "process.env.PORT" : env.PORT,
+      "process.env.PUBLIC_API_URL": JSON.stringify(env.PUBLIC_API_URL),
+    },
+    plugins: [react()],
+  }
+});
