08973296fcab5d2d297bff2cf8368fc39ec1aa8e
Kejin Zhou <kejin.zhou@aalto.fi>
2025-07-16 16:14:03 +0300

    Creates entrypoint for frontend server, fixes cd workflow, adds DockCompose launch option

---
+++ client/entrypoint.ts
@@ -0,0 +1,12 @@
+import express from "npm:express";
+import cors from "npm:cors";
+
+const app = express();
+app.use(cors());
+app.use(express.static("./dist"));
+
+const PORT = process.env.FRONTEND_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Frontend server running on port ${PORT}.`);
+});
